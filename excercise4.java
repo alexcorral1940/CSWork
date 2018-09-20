045751a83650a37d6fc2106d1e452b3d454b542f
alexcorral1940 <30845126+alexcorral1940@users.noreply.github.com>
2018-09-20 08:36:51 -0700

    Add files via upload

---
+++ excercise4.java
@@ -0,0 +1,19 @@
+public class excercise4 {
+    public static void main(String[] args)
+    {
+        printSquare(3, 7);
+    }
+    public static void printSquare(int min, int max)
+    {
+        for(int i = min; i <= max; i++)
+        {
+            int k = i;
+            for (int j = min; j <= max; j++)
+            {
+                System.out.print(k);
+                k = k == max ? min : k + 1;
+            }
+            System.out.println();
+        }
+    }
+}
